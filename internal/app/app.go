0c304f9f1cfea45970cd16bf4db888c85e82d0fe
Tamás <50444646+tamas-thisisbud@users.noreply.github.com>
2020-09-05 22:56:51 +0200

    cleanup mocks

--- internal/app/app.go
+++ internal/app/app.go
@@ -34,13 +34,6 @@ type Config struct {
 	HTTPPort int
 }
 
-// Component is part of the app which runs in a goroutine
-type Component interface {
-	// Run should cleanup the component if the context is done, and should wait for any current
-	// tasks to complete before returning an error if there was one.
-	Run(ctx context.Context) error
-}
-
 var version string
 var commitHash string
 
